ceda89f957ec7c1011a00e35085130a709ed37f7
Michael Hunziker <michael.hunziker@youengineering.com>
2016-10-10 16:45:55 +0200

    http call

---
+++ src/app/shared/book-data.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+import {Http, Response} from "@angular/http";
+import {Observable} from "rxjs";
+
+@Injectable()
+export class BookDataService {
+
+  constructor(private http: Http) {
+  }
+
+  url: string = 'http://localhost:4730/books';
+
+  addBook(book) {
+    return this.http
+      .post(this.url, book);
+  }
+
+  getBooks() {
+
+    return this.http
+      .get(this.url)
+      .map((res: Response) => res.json() || {})
+      .catch(function (error: any) {
+        console.log('ERROR', error);
+        return Observable.throw(error.message)
+      });
+
+    // return [
+    //   {
+    //     title: "Buch 1",
+    //     isbn: "123",
+    //     author: "Michi"
+    //   },
+    //   {
+    //     title: "Buch 2",
+    //     isbn: "567",
+    //     author: "Hans"
+    //   }
+    // ]
+  }
+
+}
